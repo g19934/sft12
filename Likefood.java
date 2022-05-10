fa6ab70c134eeaf3a90bd31d7b91ab254e91b5bc
g19934 <g19934sy@gm.tsuda.ac.jp>
2022-05-11 02:36:43 +0900

    修正したである

--- Likefood.java
+++ Likefood.java
@@ -4,7 +4,7 @@ public class Likefood{
     String likefood="チョコレート";
     String riyu="甘いから";
 
-    System.out.println("好きな食べ物は（修正箇所である）"+likefood);
+    System.out.println("好きな食べ物は（修正watashi）"+likefood);
     System.out.println("理由は"+riyu);
   }
 }
